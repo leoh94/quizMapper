ff4a1829227f91d9a7f996003f9593569c1d7fcc
Leo Hussain <leo.94@hotmail.co.uk>
2018-05-02 18:32:15 -0400

    getDistance

--- uceshus/platforms/browser/www/js/appActivity.js
+++ uceshus/platforms/browser/www/js/appActivity.js
@@ -1,6 +1,5 @@
 // the variables
 // and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on 
-var earthquakelayer;
 // a global variable to hold the http request
 var client;
 // store the map
@@ -54,16 +53,40 @@ function loadMap(){
 
 } 
 
-// create an event detector to wait for the user's click event and then use the popup to show them where they clicked
-// note that you don't need to do any complicated maths to convert screen coordinates to real world coordiantes - the Leaflet API does this for you
-function onMapClick(e) {
-	popup
-	.setLatLng(e.latlng)
-	.setContent("You clicked the map at " + e.latlng.toString())
-	.openOn(mymap);
-	}
-	// now add the click event detector to the map
-	mymap.on('click', onMapClick);
+function getDistance() {
+	// getDistanceFromPoint is the function called once the distance has been found
+	navigator.geolocation.getCurrentPosition(getDistanceFromPoint);
+}
+
+function getDistanceFromPoint(position) {
+	// find the coordinates of a point using this website:
+	// these are the coordinates for Warren Street
+	var lat = 51.524616;
+	var lng = -0.13818;
+	// return the distance in kilometers
+	var distance = calculateDistance(position.coords.latitude, position.coords.longitude, lat,lng, 'K');
+	document.getElementById('showDistance').innerHTML = "Distance: " + distance;
+}
+
+// code adapted from https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-inyour-web-apps.html
+function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180;
+	var radlat2 = Math.PI * lat2/180;
+	var radlon1 = Math.PI * lon1/180;
+	var radlon2 = Math.PI * lon2/180;
+	var theta = lon1-lon2;
+	var radtheta = Math.PI * theta/180;
+	var subAngle = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	subAngle = Math.acos(subAngle);
+	subAngle = subAngle * 180/Math.PI; // convert the degree value returned by acos back to degrees from radians
+	dist = (subAngle/360) * 2 * Math.PI * 3956; // ((subtended angle in degrees)/360) * 2 * pi * radius )
+	// where radius of the earth is 3956 miles
+	if (unit=="K") { dist = dist * 1.609344 ;} // convert miles to km
+	if (unit=="N") { dist = dist * 0.8684 ;} // convert miles to nautical miles
+	return dist;
+}
+
+
 //function showPointLineCircle(){
 	// add a point
 	//L.marker([51.5, -0.09]).addTo(mymap).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
